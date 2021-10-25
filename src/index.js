5a90aaec65d8ae67e971a14313e67625f18f6b42
shubhasankar <ssbhaumikdmr@gmail.com>
2021-10-26 00:08:16 +0530

    feat: Initial basic set up for React app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Fundoo Notes App</h1>,
   document.getElementById('root')
 );
 
