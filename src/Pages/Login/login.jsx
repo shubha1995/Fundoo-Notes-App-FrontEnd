940c1bceaba931a33ca1cc4cd6f73a3ef92e67c8
shubhasankar <ssbhaumikdmr@gmail.com>
2021-10-30 01:38:16 +0530

    feat: Added validation for Login & Registration pages

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,8 +1,10 @@
-/* eslint-disable no-unused-vars */
-import React from "react"
+/* eslint-disable no-undef */
+import React from "react";
 import { Grid, Paper, TextField, Button, Typography, Link } from "@material-ui/core";
 import FundooHeader from '../../Components/FundooHeader/FundooHeader';
-import '../Login/login.scss'
+import { ErrorMessage ,Formik, Field, Form} from 'formik';
+import * as yup from 'yup';
+import '../Login/login.scss';
 
 const Login =() => {
     const paperStyle = {
@@ -11,25 +13,83 @@ const Login =() => {
         width: 280,
         margin: "50px auto",
     };
-    const btstyle = { margin: "8px 0", marginTop: 30, marginBottom: 30 };
+    const fieldStyle = {margin:'5vh 0'}; 
+
+
     return (
+        <Formik
+        initialValues ={{
+            emailId:'',
+            password:''
+          }}
+          validationSchema ={ yup.object({
+            emailId: yup.string().email('Invalid Email address').required('Required'),
+            password:yup.string()
+            .required('Required')
+    
+          })}
+          onSubmit = {values=>{
+            console.log(values);
+            Login(values)
+              .then(response => console.log(response ))
+              .catch(error=>console.log(error))
+          }}
+          >
+
+        {formik =>(
         <Grid>
-            <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={10} style={paperStyle} >
                 <Grid align="center">
                     <FundooHeader/>
                 <h2 className="header">Sign In</h2>
                 <h4>Use your FundooNotes Account</h4>
                 </Grid>
-                <TextField variant="outlined" label= "Email Id" placeholder= "Enter user emailId" fullWidth required margin="normal"/>
-                <TextField variant="outlined" label= "Password" placeholder= "Enter password" type="password" fullWidth required margin="normal"/>
-                <Typography>
+                <Form onSubmit={formik.handleSubmit}> 
+                <Field id="emailId"
+                      as = {TextField} 
+                      name = 'emailId'
+                      label="EmailId" 
+                      variant="outlined"
+                      onChange = {formik.handleChange}
+                      onBlur = {formik.handleBlur}
+                      value={formik.values.emailId}
+                      
+                      helperText = {<ErrorMessage name = 'emailId'/>}
+                      fullWidth
+                      required/>
+                      
+                  <Field
+                      as = {TextField}
+                      name="password"
+                      id="password" 
+                      label="Password" 
+                      type="password" 
+                      fullWidth 
+                      variant="outlined" 
+                      onChange = {formik.handleChange}
+                      helperText = {<ErrorMessage name = 'password'/>}
+                      value={formik.values.password}
+                      required  
+                      margin="normal"
+                      style = {fieldStyle}/>
+                      
+                <Typography className="forgotPass">
                     <Link href="#">Forgot password</Link>
                 </Typography>
-                <Button className="signIn"type="submit" color="primary" variant="contained" style={btstyle} fullWidth > Sign in</Button>
+                <Typography>
+                    <Button type = "Submit" 
+                    color = "primary" 
+                    variant = "contained"  
+                    className='signIn' fullWidth>
+                      Sign In</Button>
+                  </Typography>
 
                 <p className='register'><Button href='/register' color='primary' variant = 'text'>Create account</Button></p>          
+                </Form>
             </Paper>
         </Grid>
+        )}
+        </Formik>
     )
 }
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,36 +1,150 @@
 import React from 'react'
-import { Grid, Paper, TextField, Button } from '@material-ui/core'
+import { Grid, Paper, Typography, TextField, Button } from '@material-ui/core'
 import FundooHeader from '../../Components/FundooHeader/FundooHeader';
 import "../Register/Register.scss";
+import {Formik,Form,ErrorMessage,Field} from 'formik';
+import * as yup from 'yup'
 const SignUp = () => {
-    const paperStyle = { 
-        padding: '30px 20px', 
-        width: 300, 
-        margin: "20px auto",
-        height: "70vh",
-     }
-    const btstyle = { margin: "12px 0" };
-    return (
-        <Grid>
-            <Paper elevation={20} style={paperStyle}>
-                <Grid align='center'>
-                    <FundooHeader/>
-                    <h4>Create your FundooNotes Account</h4>
+
+    return(
+        
+        
+        <Paper elevation = {10} className = "paperStyle">
+            <Formik initialValues = {{
+                            firstName :'',
+                            lastName :'',
+                            emailId :'',
+                            password :'',
+                            confirmPassword:''
+                    }}
+                validationSchema ={yup.object({
+                    firstName : yup.string().required('Required'),
+                    lastName : yup.string().required('Required'),
+                    emailId: yup.string().email('Enter a valid Email').required('Required'),
+                    password: yup.string()
+                    .required('Required'),
+                    confirmPassword: yup.string()
+                    .oneOf([yup.ref('password'), null], 'Passwords must match')
+                })}
+                onSubmit = {values=>{
+                    console.log(values);
+                    SignUp(values)
+                        .then(response=> console.log(response ))
+                        .catch(error=>console.log(error));
                     
+                }}
+            >
+            {formik =>(
+                <>
+                <Grid>
+                <FundooHeader/>
+                <h4>Please Fill Details to Create an Account</h4>
                 </Grid>
-                <form>
-                    <TextField variant="outlined" fullWidth label='First Name' placeholder="Enter your First Name" margin="normal" />
-                    <TextField variant="outlined" fullWidth label='Last Name' placeholder="Enter your Last Name" margin="normal" />
-                    <TextField variant="outlined" fullWidth label='Email' placeholder="Please enter valid Email Id" margin="normal" />
-                    <TextField variant="outlined" fullWidth label='Password' type="password" placeholder="Provide your password" margin="normal" />
-                    <Button type='submit' variant='contained' color='primary' style={btstyle} fullWidth>Sign Up</Button>
-                    <Button className= "signIn" href='/login' color='primary' variant = 'text'>Sign in instead</Button>
-                    
-                </form>
                 
-            </Paper>
-        </Grid>
-    )
+                <Form onSubmit = {formik.handleSubmit}>
+                    <Grid container spacing = {0}>
+                        <Grid item  sm = {3}>
+                            <Field
+                                as = {TextField}
+                                name = "firstName"
+                                id = 'firstName'
+                                label="First Name" 
+                                variant="outlined"
+                                className="textStyle"
+                                color = 'primary'
+                                onChange = {formik.handleChange}
+                                onBlur = {formik.handleBlur}
+                                value = {formik.values.firstName}
+                                helperText = {<ErrorMessage name = "firstName"
+                                                style = {{textDecorationColor:"red"}}/>}
+                            />
+                        </Grid>
+                        <Grid item sm = {3}>
+                            <Field
+                                as = {TextField}
+                                id='lastName'
+                                label='Last Name'
+                                variant = 'outlined'
+                                onChange = {formik.handleChange}
+                                onBlur = {formik.handleBlur}
+                                value = {formik.values.lastName}
+                                helperText = {<ErrorMessage name = "lastName"/>}
+                                className="textStyle"
+                            />
+                        </Grid>
+                    </Grid>    
+                        <div className = 'box'>
+                            <Field
+                                as = {TextField}
+                                id = 'emailId'
+                                label = "Email Id"
+                                variant = "outlined"
+                                className = 'textField' 
+                                value = {formik.values.emailId}
+                                onChange = {formik.handleChange}  
+                                helperText = {<ErrorMessage name = "emailId"/>}
+                                onBlur = {formik.handleBlur}
+                            />
+                        </div>
+                        <div className = 'box'> 
+
+                        <Grid container spacing = {0}>
+                        <Grid item  sm = {3}>
+                            <Field
+                                as = {TextField}
+                                type='password'
+                                id = 'password'
+                                label = "password"
+                                variant = "outlined"
+                                className = 'textStyle'
+                                value = {formik.values.password}
+                                helperText = {<ErrorMessage name = "password"/>}
+                                onChange = {formik.handleChange}
+                                onBlur = {formik.handleBlur}                               />
+                        </Grid>
+                        <Grid item sm = {3}>
+                            <Field
+                                as = {TextField}
+                                type='password'
+                                id = 'confirmPassword'
+                                label = "confirmPassword"
+                                variant = "outlined"
+                                className = 'textStyle'
+                                value = {formik.values.confirmPassword}
+                                helperText = {<ErrorMessage name = "confirmPassword"/>}
+                                onChange = {formik.handleChange}
+                                onBlur = {formik.handleBlur}                                 
+                            />
+                        </Grid>
+                    </Grid>  
+                    </div>
+                    
+
+                        <Typography className = 'button'>
+                            <Button type = "Submit" 
+                                className = "button" 
+                                variant ="contained"
+                                color='primary' 
+                            >
+                                Register
+                            </Button>
+
+                    <p> <Button href='/login' color='primary' variant = 'text'>Sign in instead</Button></p>
+                        
+                        <div className="register-avatar">
+                        <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt=''></img>
+                        </div>
+                        </Typography>
+                        
+                          
+                </Form>
+                </>
+                )}
+            </Formik>
+        </Paper>
+    
+);
+
 }
 
 export default SignUp;
