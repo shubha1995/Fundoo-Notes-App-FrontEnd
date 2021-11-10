e65d55661092eded73e45c4c445af35465c74faf
shubhasankar <ssbhaumikdmr@gmail.com>
2021-11-10 23:44:24 +0530

    feat: Added dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch,Route } from 'react-router-dom';
 import Login from "./Pages/Login/login.jsx";
 import Register from "./Pages/Register/Register.jsx"
 import FrogotPassword from "./Pages/ForgotPassword/ForgotPassword.jsx"
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path ='/register' component={Register}/>
       <Route exact path = '/' component = {Login}/>
       <Route path ='/forgotPassword' component={FrogotPassword}/>
+      <Route path="/dashboard" component={Dashboard}/>
     </Switch>
     </BrowserRouter>
     </>
