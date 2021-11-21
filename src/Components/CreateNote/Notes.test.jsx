cd34da72710a2f54b7b7c834667ad24f9383d032
shubhasankar <ssbhaumikdmr@gmail.com>
2021-11-21 23:30:40 +0530

    feat: Added test cases for AddNotes component

--- src/Components/CreateNote/Notes.test.jsx
+++ src/Components/CreateNote/Notes.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import AllNotes from "../createNote/Notes";
+import AllNotes from "../CreateNote/Notes";
 
 it("givenTestIdElement_WhenRenderedAddNotes_ShouldContainHeaderWithExpectedInputElements", () => {
   const { queryByTestId } = render(<AllNotes />);
